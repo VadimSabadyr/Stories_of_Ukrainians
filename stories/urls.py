804bfbc52f52824f158c90b5a1764c2a4832b7e5
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-10 14:21:26 +0200

    urls app create urls name(publication-list, publication-detail, publication-create, publication-update, publication-delete, toggle-publication-assign)

--- stories/urls.py
+++ stories/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
 from .views import (
     index,
-    ManufacturerListView,
-    ManufacturerCreateView,
-    ManufacturerUpdateView,
-    ManufacturerDeleteView,
+    CityListView,
+    CityCreateView,
+    CityUpdateView,
+    CityUpdateView,
+    CityDeleteView,
+    PublicationListView,
+    PublicationDetailView,
+    PublicationCreateView,
+    PublicationUpdateView,
+    PublicationDeleteView,
+    toggle_assign_to_publication,
+
 )
+
+
+app_name = "stories"
 
 
 urlpatterns = [
@@ -29,5 +40,15 @@ urlpatterns = [
         "cities/<int:pk>/delete/",
         CityDeleteView.as_view(),
         name="city-delete",
+    ),
+    path("publications/", PublicationListView.as_view(), name="publication-list"),
+    path("publications/<int:pk>/", PublicationDetailView.as_view(), name="publication-detail"),
+    path("publications/create/", PublicationCreateView.as_view(), name="publication-create"),
+    path("publications/<int:pk>/update/", PublicationUpdateView.as_view(), name="publication-update"),
+    path("publications/<int:pk>/delete/", PublicationDeleteView.as_view(), name="publication-delete"),
+    path(
+        "cars/<int:pk>/toggle-assign/",
+        toggle_assign_to_publication,
+        name="toggle-publication-assign",
     ),
     ]
