cf8df0c7bd8618fa450790f744c234955d5b2632
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-10 12:51:49 +0200

    create model Author

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -9,3 +10,12 @@ class City(models.Model):
 
     def __str__(self):
         return {self.name}
+
+
+class Author(AbstractUser):
+    license_number = models.TextChoices("brilliant", "good")
+
+    class Meta:
+        verbose_name = "author"
+        verbose_name_plural = "authors"
+
