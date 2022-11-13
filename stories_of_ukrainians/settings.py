880cd83c711b006d8bacf130df85efe657092feb
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-13 19:15:07 +0200

    Deploying

--- stories_of_ukrainians/settings.py
+++ stories_of_ukrainians/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://stories-in-ukrainiane.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "stories-in-ukrainiane.onrender.com"]
 
 
 # Application definition
