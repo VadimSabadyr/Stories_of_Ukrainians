c7bdf320602470fd7d6d9a6e22c6139214c979d4
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-13 17:30:21 +0200

    Remove insecure Django secret key from settings.py

--- stories_of_ukrainians/settings.py
+++ stories_of_ukrainians/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+
+DEBUG = 'RENDER' not in os.environ
+# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
