3172ae201763f71e759ec12bc809cd15be79fe49
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-13 18:57:21 +0200

    Deploying

--- stories_of_ukrainians/settings.py
+++ stories_of_ukrainians/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = 'RENDER' not in os.environ
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "https://stories-in-ukrainiane.onrender.com"]
 
