0355a6d2f9d372a6eeca3ce26336be4a8a3b09db
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-16 16:04:02 +0200

    Refactoring html

--- stories_of_ukrainians/settings.py
+++ stories_of_ukrainians/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
@@ -93,8 +93,8 @@ DATABASES = {
     }
 }
 
-db_form_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_form_env)
+# db_form_env = dj_database_url.config(conn_max_age=500)
+# DATABASES["default"].update(db_form_env)
 
 
 # Password validation
